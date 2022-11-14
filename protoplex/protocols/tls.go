6acf868ec910c9c04c8e4c86e874f9eeb48f7aee
Vuong Le <lelordoftech@gmail.com>
2022-11-14 13:36:18 +0900

    Add comment code

--- protoplex/protocols/tls.go
+++ protoplex/protocols/tls.go
@@ -1,5 +1,9 @@
 package protocols
 
+// TLS_HANDSHAKE_CONTENT_TYPE = 0x16
+// TLS_VERSION_MAJOR = 0x03
+// TLS_VERSION_MINOR = 0x01
+
 // NewTLSProtocol initializes a Protocol with a TLS signature.
 func NewTLSProtocol(targetAddress string) *Protocol {
 	return &Protocol{
